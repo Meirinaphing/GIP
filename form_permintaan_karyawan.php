dc973673c4a0cbc289dd644763bb305f3b7eac23
ElvenLS <33652530+ElvenLS@users.noreply.github.com>
2018-07-26 14:36:43 +0700

    baru rapikan blm masuk database

--- form_permintaan_karyawan.php
+++ form_permintaan_karyawan.php
@@ -29,138 +29,131 @@
   </div> -->
 
 	
-    <h2 align="center">Form Permintaan Karyawan Baru</h2>
-    <form class="form-horizontal" action="#">
-      <div class="form-group" align="center">
-        <label class="control-label col-sm-offset-4 col-sm-1">No:</label>
-        <div class="col-sm-3">
-          <input type="text" class="form-control" id="noform" name="noform" readonly>
-        </div>
-      </div>
-		<br>
-      <div class="form-group row">
-        <label class="control-label col-sm-2" style="text-align: right;">Nama Pemohon:</label>
-        <div class="col-sm-4">
-          <input type="text" class="form-control" id="namapemohon" name="namapemohon" readonly>
-        </div>
-        <label class="control-label col-sm-2 col-sm-offset-3" style="text-align: right;">Tanggal:</label>
-        <div class="col-sm-4">
-          <input type="text" class="form-control" id="tglmohon" name="tglmohon" readonly>
-        </div>
-      </div>
-      <hr style="height: 1px; margin-bottom: 40px;" noshade>
 
+	<div class="content-header">
+		<div class="container-fluid">
+			<div class="row mb-2">
+				<div class="col-sm-6">
+					<h1 class="m-0 text-dark">Form Permintaan Karyawan Baru</h1>
+					<h5>N0.0812391230432984</h5>
+				</div><!-- /.col -->
+				<div class="col-sm-6">
+					<ol class="breadcrumb float-sm-right">
+						<li class="breadcrumb-item"><a href="#">Home</a></li>
+						<li class="breadcrumb-item active">Form Permintaan Karyawan Baru</li>
+					</ol>
+				</div><!-- /.col -->
+			</div><!-- /.row -->
+		</div><!-- /.container-fluid -->
+	</div>
+	<!-- /.content-header -->
 
-     <!--  <div style="margin-bottom: 10px; height: 150px;">
-        <div class="col-sm-5" style="border:1px solid grey; background-color: lightgrey;">
-          <h4 align="center"> Personalia </h4>
-          <div class="form-group">
-            <label class="control-label col-sm-2" style="text-align: left;">Nama:</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="namapersonalia" name="namapersonalia" readonly>
-            </div>
+	<!-- Main content -->
+	<section class="content" >
+		<div class="container-fluid">
+			<!-- Small boxes (Stat box) -->
+			<div class="row">
+				<div class="col-lg-12">
+					<!-- small box -->
+					<div class="card card-default">
+					  <div class="card-header">
+						<div class="row">  
+						<h3 class="card-title col-sm-6">Nama Pemohon : Leader</h3>
+						<h3 class="card-title col-sm-6" style="text-align:right;">Tanggal : <?php echo date("D").','.date('d-M-Y') ?></h3>
+						</div>
+					  </div>
+					  <!-- /.card-header -->
+					  <div class="card-body">
+						<div class="row">
+							
+							<div class="col-sm-4">
+        						<label class="control-label" style="text-align: left;">Divisi/Dept:</label>
+          						<input type="text" class="form-control" id="divisi" name="divisi" placeholder="Enter Divisi">
+        					</div>
+							<div class="col-sm-4">
+        						<label class="control-label">Job Kelas:</label>
+						  		<input type="text" class="form-control" id="job" name="job" placeholder="Enter Job Kelas">
+							</div>
+							<div class="col-sm-4">
+							  <label class="control-label" style="text-align: left;">Jabatan:</label>
+							  <input type="text" class="form-control" id="jabatan" name="jabatan" placeholder="Enter Jabatan">
+							</div>
+							
+<!--							---------------------------------------------------------------------------------------------------------------->
+							
+							  <div class="form-group col-sm-12" style="padding-top: 20px;">	
+									<div class="row">
+										<div class="col-sm-6">
+											<h4>1. Penambahan Jumlah Karyawan</h4>
+										</div>
+										<div class="col-sm-1" align="right">
+											<label class="control-label">Pria:</label>
+										</div>
+										<div class="col-sm-2">
+										  <input type="number" class="form-control form-control-sm" value="0" id="pria" name="pria" placeholder="Pria">
+										</div>
+										<div class="col-sm-1" align="right">
+											<label class="control-label ">Wanita:</label>
+										</div>
+										<div class="col-sm-2">
+										  <input type="number" class="form-control form-control-sm" value="0" id="wanita" name="wanita" placeholder="Wanita">
+										</div>
+									</div>
+							  </div>
+							
+							
+							  <div class="form-group col-sm-12">
+								<label style="margin-left:4%;">Berdasarkan Persetujuan Budget Manpower Planning:</label>
+								&nbsp&nbsp
+								<label class="control-label">
+								  <input type="radio" name="approval" value="yes" id="yes" onclick="radio()"> Yes
+								</label>
+								&nbsp&nbsp
+								<label class="control-label">
+								  <input type="radio" name="approval" value="no" id="no" onclick="radio()"> No
+								</label>
+      							<p class="co-sm-12" style="margin-left: 4%;" id="isi"></p>
+							  </div>
+							  <div class="form-group col-sm-12">
+								<label style="margin-left:4%;">Status Karyawan:</label>
+								&nbsp&nbsp
+								<label class="control-label">
+								  <input type="radio" name="status" value="bulanan" id="bulanan" onclick="fkontrak()" checked> Bulanan
+								</label>
+								&nbsp&nbsp
+								<label class="control-label">
+								  <input type="radio" name="status" value="harian" id="harian" onclick="fkontrak()"> Harian
+								</label>
+								&nbsp&nbsp
+								<label class="control-label">
+								  <input type="radio" name="status" value="kontrak" id="kontrak" onclick="fkontrak()"> Kontrak
+								  <span id="isikontrak"></span>
+								</label>
+							  </div>
+
+							  <div class="form-group col-sm-12">
+							  	<h4>2. Uraian Pekerjaan (Tugas dan Tanggung Jawab)</h4>
+								<textarea class="form-control" rows="5" id="jobdesk" style="margin-left: 4%; width: 95%;"></textarea>
+							  </div>
+
+      
+      <div class="form-group col-sm-12">
+		  <h4>3. Kualifikasi</h4>
+		  <div class="row">
+        	<label class="control-label col-sm-1" style="text-align: left; margin-left: 3%;">Umur:</label>
+          <div class="col-sm-1">
+          	<input type="text" class="form-control form-control-sm" id="umur" name="umur" placeholder="Umur">
           </div>
-          <div class="form-group">
-            <label class="control-label col-sm-2" style="text-align: left;">Tanggal:</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="tglpersonalia" name="tglpersonalia" readonly>
-            </div>
-          </div>
+        	<div class="col-sm-2" align="right">
+        	<label class="control-label">Pendidikan:</label></div>
+        	<div class="col-sm-4">
+         	 <input type="text" class="form-control form-control-sm" id="pendidikan" name="pendidikan" placeholder="Enter Pendidikan">
+        	</div>
         </div>
-        <div class="col-sm-2"></div>
-        <div class="col-sm-5" style="border:1px solid grey; background-color: lightgrey;">
-          <h4 align="center"> Personalia </h4>
-          <div class="form-group">
-            <label class="control-label col-sm-2" style="text-align: left;">Nama:</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="namapersonalia" name="namapersonalia" readonly>
-            </div>
-          </div>
-          <div class="form-group">
-            <label class="control-label col-sm-2" style="text-align: left;">Tanggal:</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="tglpersonalia" name="tglpersonalia" readonly>
-            </div>
-          </div>
-        </div>
-      </div> -->
-
-      <div class="form-group">
-        <label class="control-label col-sm-1" style="text-align: left;">Divisi/Dept:</label>
-        <div class="col-sm-4">
-          <input type="text" class="form-control" id="divisi" name="divisi" placeholder="Enter Divisi">
-        </div>
-        <label class="control-label col-sm-2 col-sm-offset-1">Job Kelas:</label>
-        <div class="col-sm-4">
-          <input type="text" class="form-control" id="job" name="job" placeholder="Enter Job Kelas">
-        </div>
+			  
       </div>
-      <div class="form-group">
-        <label class="control-label col-sm-1" style="text-align: left;">Jabatan:</label>
-        <div class="col-sm-4">
-          <input type="text" class="form-control" id="jabatan" name="jabatan" placeholder="Enter Jabatan">
-        </div>
-      </div>
-      <br>
-      <h4>1. Penambahan Jumlah Karyawan</h4>
-      <div class="form-group">
-        <label class="control-label col-sm-1">Pria:</label>
-        <div class="col-sm-1">
-          <input type="text" class="form-control" value="0" id="pria" name="pria" placeholder="Pria">
-        </div>
-        <label class="control-label col-sm-1">Wanita:</label>
-        <div class="col-sm-1">
-          <input type="text" class="form-control" value="0" id="wanita" name="wanita" placeholder="Wanita">
-        </div>
-      </div>
-      <div class="form-group">
-        <label style="margin-left:4%;">Berdasarkan Persetujuan Budget Manpower Planning:</label>
-        &nbsp&nbsp
-        <label class="control-label">
-          <input type="radio" name="approval" value="yes" id="yes" onclick="radio()"> Yes
-        </label>
-        &nbsp&nbsp
-        <label class="control-label">
-          <input type="radio" name="approval" value="no" id="no" onclick="radio()"> No
-        </label>
-      </div>
-      <p style="margin-left: 4%;" id="isi"></p>
-      <div class="form-group">
-        <label style="margin-left:4%;">Status Karyawan:</label>
-        &nbsp&nbsp
-        <label class="control-label">
-          <input type="radio" name="status" value="bulanan" id="bulanan" onclick="fkontrak()" checked> Bulanan
-        </label>
-        &nbsp&nbsp
-        <label class="control-label">
-          <input type="radio" name="status" value="harian" id="harian" onclick="fkontrak()"> Harian
-        </label>
-        &nbsp&nbsp
-        <label class="control-label">
-          <input type="radio" name="status" value="kontrak" id="kontrak" onclick="fkontrak()"> Kontrak
-          <span id="isikontrak"></span>
-        </label>
-      </div>
-
-      <h4>2. Uraian Pekerjaan (Tugas dan Tanggung Jawab)</h4>
-      <div class="form-group">
-        <textarea class="form-control" rows="5" id="jobdesk" style="margin-left: 4%; width: 95%;"></textarea>
-      </div>
-
-      <h4>3. Kualifikasi</h4>
-      <div class="form-group">
-        <label class="control-label col-sm-1" style="text-align: left; margin-left: 3%;">Umur:</label>
-        <div class="col-sm-1">
-          <input type="text" class="form-control" id="umur" name="umur" placeholder="Umur">
-        </div>
-        <div class="col-sm-2" style="width:13.5%;"></div>
-        <label class="control-label col-sm-4">Pendidikan:</label>
-        <div class="col-sm-4">
-          <input type="text" class="form-control" id="pendidikan" name="pendidikan" placeholder="Enter Pendidikan">
-        </div>
-      </div>
-      <div class="form-group">
-        <label style="margin-left:4%;">Pengalaman:</label>
+      <div class="form-group col-sm-12">
+        <label style="margin-left:3%;">Pengalaman:</label>
         &nbsp&nbsp
         <label class="control-label">
           <input type="radio" name="pengalamann" value="tidakpengalaman" id="tidakpengalaman" onclick="fpengalaman()" checked> Boleh yang tidak berpengalaman
@@ -171,30 +164,38 @@
           <span id="isipengalaman"></span>
         </label>
       </div>
+							
+      <div class="form-group col-sm-12">
       <label style="margin-left: 3%;">Kemampuan yang diharapkan:</label>
-      <div class="form-group">
         <textarea class="form-control" rows="5" id="kemampuan" style="margin-left: 4%; width: 95%;"></textarea>
       </div>
-      <div class="form-group">
+      <div class="form-group col-sm-12">
+	  <div class="row">
         <label class="control-label col-sm-1" style="text-align: left; margin-left: 3%;">Gaji(GBS):</label>
         <div class="col-sm-2">
-          <input type="text" class="form-control" id="startgaji" name="startgaji" placeholder="Start Gaji">
+          <input type="text" class="form-control form-control-sm" id="startgaji" name="startgaji" placeholder="Start Gaji">
         </div>
         <label class="col-sm-1 control-label" style="text-align: center;">s/d</label>
         <div class="col-sm-2">
-          <input type="text" class="form-control" id="endgaji" name="endgaji" placeholder="End Gaji">
+          <input type="text" class="form-control form-control-sm" id="endgaji" name="endgaji" placeholder="End Gaji">
         </div>
         <div class="col-sm-2" style="width:13.5%;"></div>
       </div>
+      </div>
 
+      <div class="form-group col-sm-12">
       <h4>4. Rencana</h4>
-      <div class="form-group">
-        <label class="control-label col-sm-2" style="margin-left: 3%;text-align: left;">Rencana Dibutuhkan:</label>
+		<div class="row">
+		<div class=" col-sm-2" style="margin-left: 3%;text-align: left;">
+        	<label class="control-label" >Rencana Dibutuhkan:</label>
+		</div>
         <div class="col-sm-4">
-          <input type="text" class="form-control" id="rencana" name="rencana" placeholder="Rencana Dibutuhkan">
+          <input type="text" class="form-control form-control-sm" id="rencana" name="rencana" placeholder="Rencana Dibutuhkan">
+        </div>
         </div>
       </div>
-      <div class="form-group">
+      <div class="form-group col-sm-12">
+      <div class="row">
         <label class="control-label col-sm-2" style="margin-left: 3%;text-align: left;">Manpower Planning Thn</label>
         <div class="col-sm-4">
           <input type="text" class="form-control" id="tahun" name="tahun" placeholder="Manpower Planning Tahun">
@@ -204,8 +205,11 @@
           <input type="text" class="form-control" id="jlhorg" name="jlhorg" placeholder="Jumlah Orang">
         </div>
         <label class="control-label" style="text-align: left;">Orang</label>
+		  
       </div>
-      <div class="form-group">
+      </div>
+      <div class="form-group col-sm-12">
+      <div class="row">
         <label class="control-label col-sm-2" style="margin-left: 3%;text-align: left;">Jumlah Karyawan Bulan</label>
         <div class="col-sm-4">
           <input type="text" class="form-control" id="jlhkkaryawan" name="jlhkkaryawan" placeholder="Jumlah Karyawan">
@@ -216,7 +220,9 @@
         </div>
         <label class="control-label" style="text-align: left;">Orang</label>
       </div>
-      <div class="form-group">
+      </div>
+      <div class="form-group col-sm-12">
+      <div class="row">
         <label class="control-label col-sm-2" style="margin-left: 3%;text-align: left;">Rencana Penambahan</label>
         <div class="col-sm-4">
           <input type="text" class="form-control" id="rencanapenambahan" name="rencanapenambahan" placeholder="Rencana Penambahan">
@@ -226,6 +232,7 @@
           <input type="text" class="form-control" id="jlhorg3" name="jlhorg3" placeholder="Jumlah Orang">
         </div>
         <label class="control-label" style="text-align: left;">Orang</label>
+      </div>
       </div>
 
 
@@ -237,9 +244,8 @@
           <button type="submit" class="btn btn-default">Submit</button>
         </div>
       </div>
-    </form>
-
-	
+<!--							---------------------------------------------------------------------------------------------------------------->
+			
   <script>
     function radio(){
       var a = 'Jika ya, Lampirkan Manpower Planning yang disetujui'
