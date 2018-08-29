ee49a3d11488ae63d7918ea1c67754b16d8e7b55
ElvenLS <33652530+ElvenLS@users.noreply.github.com>
2018-08-29 14:09:35 +0700

    ini database

--- spk.sql
+++ spk.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 31, 2018 at 03:30 AM
+-- Generation Time: Aug 29, 2018 at 09:08 AM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 5.6.24
 
@@ -23,10 +23,143 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `permintaan_kariawan`
+-- Table structure for table `pelamar`
 --
 
-CREATE TABLE `permintaan_kariawan` (
+CREATE TABLE `pelamar` (
+  `idpelamar` int(10) NOT NULL,
+  `namapelamar` varchar(100) NOT NULL,
+  `fotopelamar` text NOT NULL,
+  `tempatlahir` varchar(100) NOT NULL,
+  `tgllahir` date NOT NULL,
+  `noktp` varchar(100) NOT NULL,
+  `alamat` text NOT NULL,
+  `jabatandilamar` varchar(100) NOT NULL,
+  `jkpelamar` varchar(10) NOT NULL,
+  `statuspelamar` varchar(20) NOT NULL,
+  `kebangsaan` varchar(59) NOT NULL,
+  `notlpn` varchar(20) NOT NULL,
+  `ditempatkan` text NOT NULL,
+  `ingingaji` varchar(15) NOT NULL,
+  `adakeluarga` text NOT NULL,
+  `pernahlamar` text NOT NULL,
+  `pelanggaran` text NOT NULL,
+  `perusahaanlain` text NOT NULL,
+  `mulaikerja` text NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelamar_bahasa`
+--
+
+CREATE TABLE `pelamar_bahasa` (
+  `nobahasa` int(10) NOT NULL,
+  `idpelamar` int(10) NOT NULL,
+  `nama_bahasa` varchar(50) NOT NULL,
+  `lisan` varchar(50) NOT NULL,
+  `menulis` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelamar_kerabat`
+--
+
+CREATE TABLE `pelamar_kerabat` (
+  `idkerabat` int(10) NOT NULL,
+  `idpelamar` int(10) NOT NULL,
+  `nama_k` varchar(100) NOT NULL,
+  `telp_k` varchar(100) NOT NULL,
+  `posisi_k` varchar(100) NOT NULL,
+  `perusahaan_k` varchar(100) NOT NULL,
+  `hubungan_k` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelamar_nama`
+--
+
+CREATE TABLE `pelamar_nama` (
+  `idnama` int(10) NOT NULL,
+  `idpelamar` int(10) NOT NULL,
+  `nama_penting` varchar(100) NOT NULL,
+  `hubungan_penting` varchar(100) NOT NULL,
+  `telp_penting` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelamar_pelatihan`
+--
+
+CREATE TABLE `pelamar_pelatihan` (
+  `nopelatihan` int(10) NOT NULL,
+  `idpelamar` int(10) NOT NULL,
+  `nama_pelatihan` varchar(100) NOT NULL,
+  `penyelenggaraan_pelatihan` varchar(100) NOT NULL,
+  `tgl_pelatihan` date NOT NULL,
+  `keterangan_pelatihan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelamar_pendidikan`
+--
+
+CREATE TABLE `pelamar_pendidikan` (
+  `idpelamar` int(10) NOT NULL,
+  `namasd` varchar(100) NOT NULL,
+  `jurusansd` varchar(100) NOT NULL,
+  `ipksd` varchar(10) NOT NULL,
+  `tglmasuksd` date NOT NULL,
+  `tglselesaisd` date NOT NULL,
+  `namasmp` varchar(100) NOT NULL,
+  `jurusansmp` varchar(100) NOT NULL,
+  `ipksmp` varchar(10) NOT NULL,
+  `tglmasuksmp` date NOT NULL,
+  `tglselesaismp` date NOT NULL,
+  `namasma` varchar(100) NOT NULL,
+  `jurusansma` varchar(100) NOT NULL,
+  `ipksma` varchar(10) NOT NULL,
+  `tglmasuksma` date NOT NULL,
+  `tglselesaisma` date NOT NULL,
+  `namauniversiatas` varchar(100) NOT NULL,
+  `jurusanuniversiatas` varchar(100) NOT NULL,
+  `tglmasukuniversiatas` date NOT NULL,
+  `tglselesaiuniversiatas` date NOT NULL,
+  `ipkuniversiatas` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelamar_saudara`
+--
+
+CREATE TABLE `pelamar_saudara` (
+  `nosaudara` int(10) NOT NULL,
+  `idpelamar` int(10) NOT NULL,
+  `nama_s` varchar(100) NOT NULL,
+  `umur_s` varchar(100) NOT NULL,
+  `pendidikan_s` varchar(100) NOT NULL,
+  `keterangan_s` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permintaan_karyawan`
+--
+
+CREATE TABLE `permintaan_karyawan` (
   `nopk` varchar(50) NOT NULL,
   `iduser` varchar(50) NOT NULL,
   `tgl` date NOT NULL,
@@ -41,6 +174,7 @@ CREATE TABLE `permintaan_kariawan` (
   `status_karyawan_k` int(5) NOT NULL,
   `uraian_p` text NOT NULL,
   `umur` int(3) NOT NULL,
+  `pendidikan` varchar(20) NOT NULL,
   `pengalaman` varchar(50) NOT NULL,
   `pengalaman_y` int(5) NOT NULL,
   `kemampuan` text NOT NULL,
@@ -67,19 +201,62 @@ CREATE TABLE `user` (
   `nama` varchar(100) NOT NULL,
   `jabatan` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `password` text NOT NULL
+  `password` text NOT NULL,
+  `username` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`iduser`, `nama`, `jabatan`, `email`, `password`) VALUES
-(1, 'admin', 'admin', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3');
+INSERT INTO `user` (`iduser`, `nama`, `jabatan`, `email`, `password`, `username`) VALUES
+(1, 'admin', 'admin', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3', 'admin');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `pelamar`
+--
+ALTER TABLE `pelamar`
+  ADD PRIMARY KEY (`idpelamar`);
+
+--
+-- Indexes for table `pelamar_bahasa`
+--
+ALTER TABLE `pelamar_bahasa`
+  ADD PRIMARY KEY (`nobahasa`);
+
+--
+-- Indexes for table `pelamar_kerabat`
+--
+ALTER TABLE `pelamar_kerabat`
+  ADD PRIMARY KEY (`idkerabat`);
+
+--
+-- Indexes for table `pelamar_nama`
+--
+ALTER TABLE `pelamar_nama`
+  ADD PRIMARY KEY (`idnama`);
+
+--
+-- Indexes for table `pelamar_pelatihan`
+--
+ALTER TABLE `pelamar_pelatihan`
+  ADD PRIMARY KEY (`nopelatihan`);
+
+--
+-- Indexes for table `pelamar_pendidikan`
+--
+ALTER TABLE `pelamar_pendidikan`
+  ADD PRIMARY KEY (`idpelamar`);
+
+--
+-- Indexes for table `pelamar_saudara`
+--
+ALTER TABLE `pelamar_saudara`
+  ADD PRIMARY KEY (`nosaudara`);
 
 --
 -- Indexes for table `user`
@@ -91,6 +268,36 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `pelamar`
+--
+ALTER TABLE `pelamar`
+  MODIFY `idpelamar` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pelamar_bahasa`
+--
+ALTER TABLE `pelamar_bahasa`
+  MODIFY `nobahasa` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pelamar_kerabat`
+--
+ALTER TABLE `pelamar_kerabat`
+  MODIFY `idkerabat` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pelamar_nama`
+--
+ALTER TABLE `pelamar_nama`
+  MODIFY `idnama` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pelamar_pelatihan`
+--
+ALTER TABLE `pelamar_pelatihan`
+  MODIFY `nopelatihan` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pelamar_saudara`
+--
+ALTER TABLE `pelamar_saudara`
+  MODIFY `nosaudara` int(10) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `user`
 --
